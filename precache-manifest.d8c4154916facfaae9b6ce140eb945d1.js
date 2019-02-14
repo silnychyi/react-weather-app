ce19a24ff0c12091f43e925203c653520e1967ba
silnychyi <silnichii@gmail.com>
2019-02-14 17:20:16 +0100

    Updates

---
+++ precache-manifest.d8c4154916facfaae9b6ce140eb945d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7282389e8a8c7a1b9da4ad131915e509",
+    "url": "/react-weather-app/static/media/bg1.7282389e.jpeg"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "cf72e07e82c7aca29486",
+    "url": "/react-weather-app/static/js/main.cf72e07e.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-weather-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "cf72e07e82c7aca29486",
+    "url": "/react-weather-app/static/css/main.4be0e0f9.chunk.css"
+  },
+  {
+    "revision": "db1a023358b5796f05722e8b2cb50a94",
+    "url": "/react-weather-app/index.html"
+  }
+];
