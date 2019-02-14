206ffa1c893e452646c18be85ddc55ffeb358daa
silnychyi <silnichii@gmail.com>
2019-02-15 00:05:26 +0100

    Updates

--- precache-manifest.58fde3dbb3102eed2df11897be3a4018.js
+++ precache-manifest.58fde3dbb3102eed2df11897be3a4018.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
   },
   {
-    "revision": "24d48cfcd0bf9aa4388b",
-    "url": "/react-weather-app/static/js/main.24d48cfc.chunk.js"
+    "revision": "52d8873fb1e7385c941f",
+    "url": "/react-weather-app/static/js/main.52d8873f.chunk.js"
   },
   {
     "revision": "4a34e45d34cafb7623bd",
     "url": "/react-weather-app/static/js/1.4a34e45d.chunk.js"
   },
   {
-    "revision": "24d48cfcd0bf9aa4388b",
-    "url": "/react-weather-app/static/css/main.b21f2685.chunk.css"
+    "revision": "52d8873fb1e7385c941f",
+    "url": "/react-weather-app/static/css/main.46c672cc.chunk.css"
   },
   {
-    "revision": "4b6577d0de667eb8ec81fd29933617a3",
+    "revision": "02ac4335370ace24b49f4931b70e251e",
     "url": "/react-weather-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-weather-app/precache-manifest.09301ba6301ef07ec8affb87720fdd2b.js"
+  "/react-weather-app/precache-manifest.58fde3dbb3102eed2df11897be3a4018.js"
 );
 
 workbox.clientsClaim();
